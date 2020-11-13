326a69bed2281cd5f680f7cd1f87344c6a578e79
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-13 16:33:38 +0900

    active_hashのモデル作成とビューを一部修正

---
+++ app/models/timeframe.rb
@@ -0,0 +1,15 @@
+class Timeframe < ApplicationRecord
+  self.data = [
+    { id: 1, type: '___' },
+    { id: 2, type: '平日: 朝方' },
+    { id: 3, type: '平日: 昼時' },
+    { id: 4, type: '平日: 夕方' },
+    { id: 5, type: '平日: 夜中' },
+    { id: 6, type: '休日: 朝方' },
+    { id: 7, type: '休日: 昼時' },
+    { id: 8, type: '休日: 夕方' },
+    { id: 9, type: '休日: 夜中' },
+    { id: 10, type: '特に決まっていない' }
+  ]
+  
+end
---
+++ spec/factories/timeframes.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :timeframe do
+    
+  end
+end
