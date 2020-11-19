e8bbb290c9ac35261db6836d26915091978f5a0f
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-19 12:35:36 +0900

    searchビューまで定義

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'routines#index'
   resources :routines do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end
