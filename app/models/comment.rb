7f7335f8fa8def99da973398281ab620e8154bdd
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-25 12:48:00 +0900

    テストコード記述 firstcommit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :routine
   belongs_to :user
 
-  validates :text, presence: true, length: { maximum: 25 }
+  validates :text, presence: true, length: { maximum: 50 }
 end
