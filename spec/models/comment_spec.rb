d5a3fe4aacad9a0d6dc4fb9efc147cd621cd3043
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-25 16:07:06 +0900

    commentモデルの単体テスト確認

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  describe '#create' do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    it "textがあれば保存できること" do
+      expect(@comment).to be_valid
+    end
+
+    it "textがなければ保存できないこと" do
+      @comment.text = nil
+      @comment.valid?
+    end
+  end
 end
